07aa4f6182de97683f94b2bdebc349fc25d3aecf
Naveen Neelam <neelamnaveencs@gmail.com>
2024-05-24 17:03:22 +0530

    build html

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ app.use(router);
 if(process.env.NODE_ENV==="production"){
   app.use(express.static('../frontend/build'));
   app.get("*",(req, res) =>{
-      res.sendFile(path.resolve(__dirname,'frontend', 'build', 'index.html'));
+      res.sendFile('../frontend/build/index.html');
   })
 }
 
